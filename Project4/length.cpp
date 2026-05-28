84cd1617b295eb140685f53cb79f08d0529b772e
lisanghyun <lisanghyun@gmail.com>
2026-05-28 16:20:28 +0900

    글자 수 검사 function

--- Project4/length.cpp
+++ Project4/length.cpp
@@ -1,3 +1,20 @@
-class length {
+#include <iostream>
+#include <string>
+#include <algorithm>
 
+using namespace std;
+
+class Length {
+public:
+	double calLengthScore(string s1, string s2) {
+		int lenA = max(s1.size(), s2.size());
+		int lenB = min(s1.size(), s2.size());
+
+		double score = 0.0;
+
+		if (lenA < 2 * lenB) {
+			score = (1.0 - (double)(lenA - lenB) / lenB) * 60.0;
+		}
+		return score;
+	}
 };
